de2d3f3fdf445b05259a5846e138057bb28279da
aaronfeingold <ajfeingold88@gmail.com>
2024-08-26 17:23:37 -0400

    obey the rules...plus reformatting of the About component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,40 +1,52 @@
 import React from 'react';
 
-const about = (props) => {
+const About = (props) => {
+  let image;
+  let bio;
+  let email;
+  let resumeDownload;
+  if (props.data) {
+    ({ image, bio, email, resumeDownload } = props.data);
+  }
+  let profilepic = `images/${image}`;
 
-   let image; let bio; let email; let resumeDownload;
-   if (props.data) {
-      ({ image, bio, email, resumeDownload } = props.data)
-   }
-   let profilepic = `images/${image}`;
-
-   return (
-      <section id="about">
-         <div className="row">
-            <div className="three columns">
-               <img className="profile-pic"  src={profilepic} alt="Aaron Feingold Profile Pic" />
+  return (
+    <section id="about">
+      <div className="row">
+        <div className="three columns">
+          <img
+            className="profile-pic"
+            src={profilepic}
+            alt="Aaron Feingold Profile Pic"
+          />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
+          <p>{bio}</p>
+          <div className="row">
+            <div className="columns contact-details">
+              <h2>Contact Details</h2>
+              <p className="address">
+                <span>{email}</span>
+              </p>
             </div>
-            <div className="nine columns main-col">
-               <h2>About Me</h2>
-               <p>{bio}</p>
-               <div className="row">
-                  <div className="columns contact-details">
-                     <h2>Contact Details</h2>
-                     <p className="address">
-                        <span>{email}</span>
-                     </p>
-                  </div>
-                  <div className="columns download">
-                     <p>
-                        <a href={resumeDownload} className="button" target="_blank" rel="noopener noreferrer">
-                           <i className="fa fa-download"></i>Download Resume</a>
-                     </p>
-                  </div>
-               </div>
+            <div className="columns download">
+              <p>
+                <a
+                  href={resumeDownload}
+                  className="button"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-download"></i>Download Resume
+                </a>
+              </p>
             </div>
-         </div>
-      </section>
-   );
-}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default about;
+export default About;
