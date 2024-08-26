d81fdeafdc2bd4d54f85ab922e9f0d1d1dc41c7d
aaronfeingold <ajfeingold88@gmail.com>
2024-08-26 17:25:14 -0400

    obey the rules of Portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const portfolio = (props) => {
-
+const Portfolio = (props) => {
   let projects;
 
-  if(props.data){
-    projects = props.data.projects.map(function(projects) {
+  if (props.data) {
+    projects = props.data.projects.map(function (projects) {
       let projectImage = `images/portfolio/${projects.image}`;
 
       return (
@@ -15,32 +14,37 @@ const portfolio = (props) => {
               <a href={projects.url} title={projects.title}>
                 <img alt={projects.title} src={projectImage} />
                 <div className="overlay">
-                    <div className="portfolio-item-meta">
-                  <h5>{projects.title}</h5>
-                      <p>{projects.category}</p>
-                    </div>
+                  <div className="portfolio-item-meta">
+                    <h5>{projects.title}</h5>
+                    <p>{projects.category}</p>
                   </div>
-                <div className="link-icon"><i className="fa fa-link"></i></div>
+                </div>
+                <div className="link-icon">
+                  <i className="fa fa-link"></i>
+                </div>
               </a>
             </div>
           </div>
         </>
-      )
-    })
+      );
+    });
   }
 
   return (
-      <section id="portfolio">
+    <section id="portfolio">
       <div className="row">
-         <div className="twelve columns collapsed">
-            <h1>Check Out Some of My Work.</h1>
-            <div id="portfolio-wrapper" className="bgrid-halves s-bgrid-thirds cf">
-                {projects}
-            </div>
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Work.</h1>
+          <div
+            id="portfolio-wrapper"
+            className="bgrid-halves s-bgrid-thirds cf"
+          >
+            {projects}
           </div>
+        </div>
       </div>
-   </section>
+    </section>
   );
-}
+};
 
-export default portfolio;
+export default Portfolio;
