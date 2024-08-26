a2f534149195a212f3d3147c2e2586ee4941386b
aaronfeingold <ajfeingold88@gmail.com>
2024-08-26 17:24:38 -0400

    obey the rules of the Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,35 +1,44 @@
 import React from 'react';
 
-const footer = (props) => {
+const Footer = (props) => {
   let { social } = props.data ? props.data : "";
-  let networks
-  if(props.data){
-    networks= social.map(function(network){
-      return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-    })
+  let networks;
+  if (props.data) {
+    networks = social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
   }
 
   return (
     <footer>
-     <div className="row">
+      <div className="row">
         <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
-           <ul className="copyright">
-              <li>&copy; Copyright 2021 Aaron Feingold</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
+          <ul className="social-links">{networks}</ul>
+          <ul className="copyright">
+            <li>&copy; Copyright 2021 Aaron Feingold</li>
+            <li>
+              Design by{" "}
+              <a title="Styleshout" href="http://www.styleshout.com/">
+                Styleshout
+              </a>
+            </li>
+          </ul>
         </div>
         <div id="go-top">
           <a className="smoothscroll" title="Back to Top" href="#home">
             <i className="icon-up-open"></i>
           </a>
         </div>
-     </div>
+      </div>
     </footer>
   );
-}
+};
 
 
-export default footer;
+export default Footer;
