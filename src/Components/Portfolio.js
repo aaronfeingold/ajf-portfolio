8a98d4b2e93c8b8369075e657f16dde1f48fadb4
aaronfeingold <ajfeingold88@gmail.com>
2024-08-27 17:56:55 -0400

    no need for a partial, just return a div

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -6,24 +6,22 @@ const Portfolio = ({ data }) => {
         let projectImage = `images/portfolio/${proj.image}`;
 
         return (
-          <>
-            <div key={proj.title} className="columns portfolio-item">
-              <div className="item-wrap">
-                <a href={proj.url} title={proj.title}>
-                  <img alt={proj.title} src={projectImage} />
-                  <div className="overlay">
-                    <div className="portfolio-item-meta">
-                      <h5>{proj.title}</h5>
-                      <p>{proj.category}</p>
-                    </div>
+          <div key={proj.title} className="columns portfolio-item">
+            <div className="item-wrap">
+              <a href={proj.url} title={proj.title}>
+                <img alt={proj.title} src={projectImage} />
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>{proj.title}</h5>
+                    <p>{proj.category}</p>
                   </div>
-                  <div className="link-icon">
-                    <i className="fa fa-link"></i>
-                  </div>
-                </a>
-              </div>
+                </div>
+                <div className="link-icon">
+                  <i className="fa fa-link"></i>
+                </div>
+              </a>
             </div>
-          </>
+          </div>
         );
       })
     : [];
