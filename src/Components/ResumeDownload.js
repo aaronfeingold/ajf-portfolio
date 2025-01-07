018c5395625b78a2100674ced3e41a2a0d69e613
aaronfeingold <ajfeingold88@gmail.com>
2025-01-07 17:43:12 -0500

    throw error if download fails, setError state to true, and then redner the component

--- src/Components/ResumeDownload.js
+++ src/Components/ResumeDownload.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import ErrorModal from "./ErrorModal";
 
 const ResumeDownload = () => {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({ show: false, message: "" });
 
   const handleDownload = async () => {
     setLoading(true);
@@ -9,23 +11,35 @@ const ResumeDownload = () => {
       const response = await fetch(process.env.REACT_APP_R2_WORKER_URL);
       const data = await response.json();
       if (data.url) {
-        // Redirect to the signed URL to download the file
         window.location.href = data.url;
       } else {
-        console.error("Failed to fetch signed URL");
+        throw new Error("Failed to fetch download URL");
       }
     } catch (error) {
       console.error("Error downloading resume:", error);
+      setError({
+        show: true,
+        message:
+          "Unable to download resume. Please try again later or contact me directly.",
+      });
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <button onClick={handleDownload} disabled={loading}>
-      <i className="fa fa-download"></i>{" "}
-      {loading ? "Preparing Download..." : "Download Resume"}
-    </button>
+    <>
+      <button onClick={handleDownload} disabled={loading}>
+        <i className="fa fa-download"></i>{" "}
+        {loading ? "Preparing Download..." : "Download Resume"}
+      </button>
+
+      <ErrorModal
+        isOpen={error.show}
+        onClose={() => setError({ show: false, message: "" })}
+        message={error.message}
+      />
+    </>
   );
 };
 
