7682928133bf17dd13534976c222e1d8efb137fe
aaron.feingold <ajfeingold88@gmail.com>
2025-02-24 16:19:47 -0600

    feature: the header component will hand getting data for images and passing down those with defaults and othe settings to the background carousel

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,23 @@
-// src/Components/Header.js
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import NavBar from "./NavBar";
 import BackgroundCarousel from "./BackgroundCarousel";
-import "./BackgroundCarousel.css";
+import { ResumeContext } from "./ResumeContext";
 
 const Header = ({ data }) => {
+  const { loading } = useContext(ResumeContext);
+
   const {
-    name = "Your Name",
-    description = "Your Description",
+    name = "Aaron Feingold",
+    description = "guitarist, sommelier, and software engineer",
     social = [],
     backgroundImages = [
+      "/images/header-bg.jpg",
       "/images/header-bg-1.jpg",
       "/images/header-bg-2.jpg",
-      "/images/header-bg-3.jpg",
-    ], // Default images (replace with your actual images)
+      "/images/header-bg-3-founder.jpg",
+    ],
+    defaultBackground = "/images/testimonials-bg.jpg",
+    imageSettings = {},
   } = data || {};
 
   const networks = Array.isArray(social)
@@ -26,7 +30,6 @@ const Header = ({ data }) => {
       ))
     : null;
 
-  // useEffect to add class after a delay
   useEffect(() => {
     const timer = setTimeout(() => {
       const headline = document.querySelector(".responsive-headline");
@@ -34,16 +37,20 @@ const Header = ({ data }) => {
         headline.classList.add("responsive");
       }
     }, 100);
-    // Clean up the timer on component unmount
+
     return () => clearTimeout(timer);
-  }, []); // Empty dependency array means this effect runs once on mount
+  }, []);
 
   return (
     <header id="home">
       <NavBar />
-      {/* Add the BackgroundCarousel component */}
-      <BackgroundCarousel images={backgroundImages} interval={7000} />
-      {/* Optional overlay for better text readability */}
+      <BackgroundCarousel
+        images={backgroundImages}
+        interval={2500}
+        isLoading={loading}
+        defaultBackground={defaultBackground}
+        imageSettings={imageSettings}
+      />
       <div className="carousel-overlay"></div>
 
       <div className="row banner">
