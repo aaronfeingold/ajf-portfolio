7fe3aea929178e36ab1baea2d90515d16720276f
aaron.feingold <ajfeingold88@gmail.com>
2025-04-16 17:00:50 -0500

    use app router v5.3.0 for react v17 to rout to new paths of page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useContext, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ResumeProvider, ResumeContext } from "./Components/ResumeContext";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Portfolio from "./Components/Portfolio";
+import ProjectDetail from "./Components/ProjectDetail";
 import wetWilly from "./ASCII/wetWilly";
 
-const App = () => {
+const MainContent = () => {
   const {
     resumeData: { main, resume, portfolio },
     loading,
@@ -18,7 +20,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
+    <>
       {loading && (
         <div className="loading-overlay">
           <div className="loading-content">
@@ -36,15 +38,23 @@ const App = () => {
       <Resume data={resume} />
       <Portfolio data={portfolio} />
       <Footer data={main} />
-    </div>
+    </>
   );
 };
 
-// Wrap App component with ResumeProvider
-const AppWrapper = () => (
-  <ResumeProvider>
-    <App />
-  </ResumeProvider>
-);
+const App = () => {
+  return (
+    <ResumeProvider>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={MainContent} />
+            <Route path="/project/:projectId" component={ProjectDetail} />
+          </Switch>
+        </div>
+      </Router>
+    </ResumeProvider>
+  );
+};
 
-export default AppWrapper;
+export default App;
